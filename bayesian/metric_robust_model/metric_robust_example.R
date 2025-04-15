382ab49988200fec1c5caffd388b954f1b031999
j61327ac <anachristinaac@gmail.com>
2025-04-15 17:28:37 +0100

    Generating data using uniform distribution to test the script and updated some parameters and comments.

--- bayesian/metric_robust_model/metric_robust_example.R
+++ bayesian/metric_robust_model/metric_robust_example.R
@@ -3,16 +3,33 @@ rm(list=ls())
 
 source("metric_robust_functions.R")
 
-# Loading data:
-load("data_metric_robust_example.Rdata")
-analysis_options = c("paired", "independent") # choose one option
-analysis = analysis_options[2]
+# Generating a data set using a uniform distribution just to test the script:
+datFrm = data.frame (resp=integer(), cond=integer(), subID=integer())
+nG = 2 # number of groups
+nS = 30 # number of subjects
+minValue = 40
+maxValue = 120
+for (group in 1:nG) {
+  for (subject in 1:nS) {
+    y = runif(1, min=minValue, max=maxValue) # sampling from uniform distribution
+    datFrm[nrow(datFrm) + 1,] <- c(y, group, subject)
+  }
+}
+gName = "cond"
+yName = "resp"
+sName = "subID"
 
-# Prefix for results files names and file type:
-fileNameRoot = "results"
+# Choosing type of analysis ("paired" or "independent"):
+analysis = "independent"
+
+# Defining directory for results and prefix of the files names:
+fileNameRoot = "test"
 dir.create(fileNameRoot)
 fileNameRoot = paste0(fileNameRoot, "/", fileNameRoot)
 graphFileType = "pdf"
+
+diagnostics = TRUE
+computeEffsz = TRUE
 
 # Some parameters for the plots:
 groupNames = c("1","2") # names of the groups for independent analysis
@@ -29,7 +46,7 @@ if (analysis == "paired") {
   compValSigma = NULL # real value to show in posterior plot
   compValNu = NULL # comparative value to show in posterior plot
   compValEff = NULL # real value to show in posterior plot
-  ropeEffSz = c(-0.1, 0.1)
+  ropeEffSz = c(-0.1, 0.1) 
   # Extra parameters:
   compValMuDiff = NULL
   compValSigmaDiff = NULL
@@ -37,15 +54,14 @@ if (analysis == "paired") {
 
 # Independent analysis (two groups):
 if (analysis == "independent") {
-  compValMu = NULL # real values to show in posterior plots
-  compValMuDiff = 0.0 # real value to show in posterior plots
-  compValSigma = NULL # real values to show in posterior plots
-  compValSigmaDiff = NULL # real value to show in posterior plots
-  compValNu = NULL # comparative values to show in posterior plots
-  compValEff = NULL # real value to show in posterior plots
-  ropeEffSz = c(-0.1, 0.1)
-  # Extra parameters:
-  nullHypValMu = NULL 
+  compValMu = NULL # comparison value to show in plots of means
+  compValMuDiff = 0.0 # comparison value to show in plot of means difference
+  compValSigma = NULL # comparison value to show in plots of scales
+  compValSigmaDiff = NULL # comparison value to show in plot of scales difference
+  compValNu = NULL # comparison value to show in plots of normality
+  compValEff = NULL # comparison value to show in plot of effect size
+  ropeEffSz = c(-0.1, 0.1) # ROPE around the null value of effect size
+  nullValEff = 0 # null value to calculate effect size
 }
 
 # Getting number of groups from data:
@@ -70,18 +86,19 @@ if (nG == 2) {
 # Generating MCMC chain:
 codaSamples = genMCMC(datFrm, muPrior=muPrior, muSdPrior=muSdPrior, 
                       sigmaPriorLow=sigmaPriorLow, sigmaPriorHigh=sigmaPriorHigh,
-                      saveName=fileNameRoot, yName="resp")
+                      saveName=fileNameRoot, yName=yName)
 
 # Summary and diagnostics of the MCMC chain:
-smryMCMC(codaSamples, nG=nG, nullHypValMu=nullHypValMu, saveName=fileNameRoot, 
-         diagnostics=FALSE, graphFileType=graphFileType)
+smryMCMC(codaSamples, nG=nG, nullValEff=nullValEff, saveName=fileNameRoot, 
+         diagnostics=diagnostics, graphFileType=graphFileType, 
+         computeEffsz=computeEffsz)
 
 # Plotting the posterior distributions:
-plotMCMC(codaSamples, datFrm, nullHypValMu=nullHypValMu, compValMu=compValMu, 
+plotMCMC(codaSamples, datFrm, nullValEff=nullValEff, compValMu=compValMu, 
          compValMuDiff=compValMuDiff, compValSigma=compValSigma,
          compValSigmaDiff=compValSigmaDiff, compValNu=compValNu, 
          compValEff=compValEff, ropeEffSz=ropeEffSz, graphFileType=graphFileType,
-         saveName=fileNameRoot, groupNames=groupNames, yName="resp", 
-         subscript=subscript, subsEffsz=subsEffsz)
+         saveName=fileNameRoot, groupNames=groupNames, yName=yName, 
+         subscript=subscript, subsEffsz=subsEffsz, plotEffsz=computeEffsz)
 
 graphics.off()
